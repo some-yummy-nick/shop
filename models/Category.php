031f12ee631709d4faa9f1fa3331809a24b53ef2
Marat <yeffasol@mail.ru>
2019-07-24 17:14:52 +0300

    Add first commit

---
+++ models/Category.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marat
+ * Date: 24.07.2019
+ * Time: 17:09
+ */
+
+namespace app\models;
+use yii\db\ActiveRecord;
+
+class Category extends ActiveRecord
+{
+
+}
